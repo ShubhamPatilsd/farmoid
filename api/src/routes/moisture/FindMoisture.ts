55b7def002d1e0f711e49be0b8340bdf52e7ca27
Shubham Patil <shubhampatilsd@gmail.com>
2021-11-29 21:51:29 -0800

    added better search support

--- api/src/routes/moisture/FindMoisture.ts
+++ api/src/routes/moisture/FindMoisture.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../../database/db";
 
 export const FindMoisture = () => {
   const router = Router();
@@ -9,7 +7,7 @@ export const FindMoisture = () => {
     const query = req.query.plant;
     const searchResult = await prisma.plantInfo.findMany({
       where: {
-        name: { contains: query.toString() },
+        name: { contains: query.toString().toLowerCase() },
       },
     });
 
