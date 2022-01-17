fff03d7efc6a6e8b11021cf9fb227e5441e11c54
Shubham Patil <shubhampatilsd@gmail.com>
2022-01-16 19:39:39 -0800

    finally got carousel and center align working

--- mobile/components/GardenHomeTile.tsx
+++ mobile/components/GardenHomeTile.tsx
@@ -17,7 +17,7 @@ interface GardenHomeTileProps {
   name: string;
 }
 
-const paddingNum = 20;
+const paddingNum = 10;
 
 export function GardenHomeTile<GardenHomeTileProps>({ uri, name }) {
   let [fontsLoaded] = useFonts({
@@ -32,29 +32,53 @@ export function GardenHomeTile<GardenHomeTileProps>({ uri, name }) {
     return (
       <View
         style={{
-          padding: paddingNum,
+          // padding: paddingNum,
+          // marginRight: paddingNum * ,
+          display: "flex",
+          alignItems: "center",
         }}
       >
         <Image
           source={{ uri }}
           style={{
-            width: imageW * ((100 - paddingNum / 2) * 0.01),
-            height: width * 0.4,
+            width: width * 0.78,
+            height: width * 0.5,
+            // marginRight: ,
             resizeMode: "cover",
             borderRadius: 25,
           }}
         />
-        <Text
+        <View
           style={{
-            fontSize: 22,
-            color: "#000",
-            fontFamily: "Inter_600SemiBold",
-            textAlign: "center",
-            marginTop: 5,
+            transform: [{ translateY: -50 }],
+            backgroundColor: "#fff",
+            padding: 20,
+            borderRadius: 15,
+            width: width * 0.65,
+            // borderColor: "lightgrey",
+            // borderWidth: 1,
+            shadowColor: "#0C9359",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4.65,
+            elevation: 7,
           }}
         >
-          {name}
-        </Text>
+          <Text
+            style={{
+              fontSize: 22,
+              color: "#000",
+              fontFamily: "Inter_600SemiBold",
+              textAlign: "center",
+              marginTop: 5,
+            }}
+          >
+            {name}
+          </Text>
+        </View>
       </View>
     );
   }
--- mobile/screens/Home.tsx
+++ mobile/screens/Home.tsx
@@ -17,23 +17,27 @@ import {
 import { useEffect, useRef, useState } from "react";
 import React from "react";
 import { GardenHomeTile } from "../components/GardenHomeTile";
-import { Paginator } from "../components/GardenPaginator";
+// import { Paginator } from "../components/GardenPaginator";
 const { width, height } = Dimensions.get("screen");
 
 export function HomeScreen() {
-  const data = [
-    // "https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200",
-    // "https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200",
+  let data: any[] = [
+    "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200",
+    "https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200",
     "https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
     "https://images.unsplash.com/photo-1571192776145-9f563c1df686?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1547389432-95b8f3c47f3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z2FyZGVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1559749284-7a6971fd798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGdhcmRlbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   ];
+
+  data = [
+    { key: "left-spacer", spacer: true },
+    ...data,
+    { key: "right-spacer", spacer: true },
+  ];
+
+  const ITEM_SIZE = width * 0.82;
+  const SPACER_ITEM_SIZE = (width - ITEM_SIZE) / 2;
 
   const handleLogout = () => {
     firebaseAuth.signOut(auth);
@@ -49,37 +53,66 @@ export function HomeScreen() {
       }
     });
   }, []);
-  const scrollX = useRef(new Animated.Value(0)).current;
   return (
     <SafeAreaView
       style={[
-        { flex: 1 },
         {
           paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+          backgroundColor: "#EDF9F5",
+          // flex: 1,
+          height: height,
         },
       ]}
     >
-      <Text style={{ fontSize: 36 }}>Hello, {user.email}</Text>
+      {/* <Text style={{ fontSize: 36 }}>Hello, {user.email}</Text> */}
+
       <FlatList
         data={data}
         keyExtractor={(_, index) => {
           return index.toString();
         }}
         horizontal
+        // style={{ height: height }}
+        // onMomentumScrollEnd={onScrollEnd}
         pagingEnabled
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          {
-            useNativeDriver: false,
-          }
-        )}
+        // scrollEventThrottle={1}
+        // onScrollBeginDrag={12}
+        disableIntervalMomentum
+        // contentContainerStyle={{
+        //   alignItems: "center",
+        // }}
+        // onScroll={Animated.event(
+        //   [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+        //   {
+        //     useNativeDriver: true,
+        //   }
+        // )}
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => {
-          return <GardenHomeTile uri={item} name="Tom's Garden" />;
+        scrollEventThrottle={5}
+        snapToInterval={ITEM_SIZE}
+        decelerationRate={0.75}
+        renderItem={({ item, index }) => {
+          if (item.spacer) {
+            return <View style={{ width: SPACER_ITEM_SIZE }} />;
+          }
+
+          return (
+            <View
+              style={{
+                width: ITEM_SIZE,
+                // height: height,
+              }}
+            >
+              <GardenHomeTile uri={item} name="Tom's Garden" />
+            </View>
+          );
         }}
       />
-      <Paginator data={data} scrollX={scrollX} />
-      <Button onPress={handleLogout} title="Logout" />
+      {/* <View style={{ display: "flex", alignItems: "center" }}>
+        <Paginator data={data} scrollX={scrollX} />
+      </View> */}
+
+      {/* <Button onPress={handleLogout} title="Logout" /> */}
     </SafeAreaView>
   );
 }
